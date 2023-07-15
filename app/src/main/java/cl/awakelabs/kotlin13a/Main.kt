d2e246c60144b38d9efaa365f147bed12e18464b
Alex Muñoz <lomejor77@gmail.com>
2023-07-14 21:25:24 -0400

    Ejercicio Kotlin 13a agregada funcion mostrar resultado con formato moneda

--- app/src/main/java/cl/awakelabs/kotlin13a/Main.kt
+++ app/src/main/java/cl/awakelabs/kotlin13a/Main.kt
@@ -1,16 +1,30 @@
 package cl.awakelabs.kotlin13a
 
+import java.text.NumberFormat
+import java.util.Currency
 import kotlin.math.ceil
 
 fun main() {
-   var resultado = suma(4.5, 9.9F)
-   var redondo = redondeo(resultado)
-    println("El resultado es $resultado")
+   val resultado = suma(4.9, 9.9F)
+   val redondo = redondeo(resultado)
+   val muestraResultado = muestraResultado(resultado)
 
     println("El resultado es $redondo")
+    println("En Pesos es $muestraResultado")
+
 }
 
 fun suma(param1:Double,param2:Float): Double = param1 + param2
 
 fun redondeo (param1: Double) = ceil(param1)
+fun muestraResultado(numero: Double) : String {
+
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency(Currency.getInstance("CLP"))
+    println(format.format(numero))
+    return format.format(numero)
+
+}
+
 
